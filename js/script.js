36495e0702e20435385e10980f58d6024964f185
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-20 17:19:59 +0100

    print_in_page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
-// ## Scaletta
-// 1. creare generatore di numeri con ciclo for
-// 2. inserire if per i fizzbuzz
-// 3. inserire if per i fizz
-// 4. inserire if per i buzz
-// 5. stampare in console
+// Print in console
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
 
+// Approccio Manipolazione del DOM
+// Individuo la row target
+const fuzzBoard = document.getElementById("fuzzboard");
+
+// Creare elemento in pagina e stilizzarlo
+const list = document.createElement("ul");
+list.classList.add("list-unstyled", "row", "justify-content-center");
+
+// ciclo for con gli if a seconda dei risultati
 for (let i = 1; i <= 100; i++) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("col-1");
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz");
+        listItem.append("FizzBuzz");
+        list.appendChild(listItem);
     } else if (i % 3 == 0) {
-        console.log("Fizz");
+        listItem.append("Fizz");
+        list.appendChild(listItem);
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        listItem.append("Buzz");
+        list.appendChild(listItem);
     } else {
-        console.log(i);
+        listItem.append(i);
+        list.appendChild(listItem);
     }
 }
+
+// stampa in pagina
+fuzzBoard.appendChild(list);
