e298dbb7ea49908c17f42648ff1dcc0cab70b87b
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-20 16:09:34 +0100

    print_in_console_completed

--- README.md
+++ README.md
@@ -20,5 +20,4 @@ Come abbiamo visto puoi  usare varie tecniche (style , className, classList)
 2. inserire if per i fizzbuzz
 3. inserire if per i fizz
 4. inserire if per i buzz
-5. data validation
-6. stampare in console
+5. stampare in console
--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@
 // 2. inserire if per i fizzbuzz
 // 3. inserire if per i fizz
 // 4. inserire if per i buzz
-// 5. data validation
-// 6. stampare in console
+// 5. stampare in console
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
