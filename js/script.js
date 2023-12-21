edf851656a8237622bcc855f86ff59382c65d3e1
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-21 10:20:42 +0100

    template_literal

--- js/script.js
+++ js/script.js
@@ -1,42 +1,62 @@
-// Print in console
+// ! Print in console
 // for (let i = 1; i <= 100; i++) {
+//     let content = i;
 //     if (i % 3 == 0 && i % 5 == 0) {
-//         console.log("FizzBuzz");
+//         content = "FizzBuzz";
 //     } else if (i % 3 == 0) {
-//         console.log("Fizz");
+//         content = "Fizz";
 //     } else if (i % 5 == 0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(i);
+//         content ="Buzz";
 //     }
+//     console.log(content);
 // }
 
-// Approccio Manipolazione del DOM
+// ! Approccio Manipolazione del DOM
 // Individuo la row target
 const fuzzBoard = document.getElementById("fuzzboard");
 
 // Creare elemento in pagina e stilizzarlo
 const list = document.createElement("ul");
-list.classList.add("list-unstyled", "row", "justify-content-center");
+list.classList.add("list-unstyled", "d-flex", "flex-wrap","justify-content-center");
 
 // ciclo for con gli if a seconda dei risultati
 for (let i = 1; i <= 100; i++) {
     const listItem = document.createElement("li");
-    listItem.classList.add("col-1");
-    if (i % 3 == 0 && i % 5 == 0) {
-        listItem.append("FizzBuzz");
-        list.appendChild(listItem);
-    } else if (i % 3 == 0) {
-        listItem.append("Fizz");
-        list.appendChild(listItem);
-    } else if (i % 5 == 0) {
-        listItem.append("Buzz");
-        list.appendChild(listItem);
-    } else {
-        listItem.append(i);
-        list.appendChild(listItem);
+    let content = i;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        content = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        content = "Fizz";
+    } else if (i % 5 === 0) {
+        content = "Buzz";
     }
+    
+    if (isNaN(content)) listItem.classList.add(content);
+    listItem.append(content);
+    list.appendChild(listItem);
+
 }
 
 // stampa in pagina
 fuzzBoard.appendChild(list);
+
+// ! template literal 
+// const list = document.getElementById("fuzzboard");
+// let items = " ";
+
+// for (let i = 1; i >= 100; i++) {
+//     let content = i;
+//     if (i % 15 === 0) {
+//         content = "FizzBuzz";
+//     } else if (i % 3 == 0) {
+//         content = "Fizz";
+//     } else if (i % 5 == 0) {
+//         content ="Buzz";
+//     }
+
+//     let className = isNaN(content) ? content : " ";
+//     items += `<li class = "${className}">${content}</li>`;
+// }
+
+// list.innerHTML = items;
